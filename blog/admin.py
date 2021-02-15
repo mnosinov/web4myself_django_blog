0ae805a35226ffe86d748959166519ef29a71e4c
mnosinov <mnosinov@gmail.com>
2021-02-15 14:47:24 +0600

    prepopulated values added to slug fields in our model classes

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 
-from blog.models import Category, Tag, Post
+from .models import Category, Tag, Post
 
 
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Post)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
