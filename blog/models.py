9182956afc098e98d7519e744d9333dfb8e40cde
mnosinov <mnosinov@gmail.com>
2021-02-15 14:33:03 +0600

    models added and admin now can see our models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='URL категории', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, verbose_name='URL тега', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='URL поста', unique=True)
+    author = models.CharField(max_length=50)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-created_at']
--- web4myself_django_blog/settings.py
+++ web4myself_django_blog/settings.py
@@ -124,3 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'web4myself_django_blog/static')
 ]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
