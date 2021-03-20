4f548fd378d3edc773742117bccd156202766304
werty <mnosinov@gmail.com>
2021-03-20 13:39:33 +0600

    Signle Post view page is ready (DetailView). Posts by tag is also ready to use (PostsByTag), basically copied from PostsByCategory and modified slightly

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import Home, get_post, PostsByCategory
+from .views import Home, PostsByCategory, GetPost, PostsByTag
 
 
 urlpatterns=[
     path('', Home.as_view(), name='home'),
     path('category/<str:slug>/', PostsByCategory.as_view(), name='category'),
-    path('post/<str:slug>/', get_post, name='post')
+    path('tag/<str:slug>/', PostsByTag.as_view(), name='tag'),
+    path('post/<str:slug>/', GetPost.as_view(), name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from blog.models import Post, Category
+from django.views.generic import ListView, DetailView
+from blog.models import Post, Category, Tag
+from django.db.models import F
 
 
 class Home(ListView):
@@ -30,13 +30,30 @@ class PostsByCategory(ListView):
         return context
 
 
-def index(request):
-    return render(request, 'blog/index.html')
+class GetPost(DetailView):
+    model = Post
+    template_name = 'blog/single.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
 
 
-def get_category(request, slug):
-    return render(request, 'blog/category.html')
+class PostsByTag(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
 
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
 
-def get_post(request, slug):
-    return render(request, 'blog/category.html')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу: ' + \
+            str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
