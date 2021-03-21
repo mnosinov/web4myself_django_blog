3de4e8d6f467a55e8d1f6dc2413320167d819d75
werty <mnosinov@gmail.com>
2021-03-21 11:05:23 +0600

    post search functionality has been added

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import Home, PostsByCategory, GetPost, PostsByTag
+from .views import Home, PostsByCategory, GetPost, PostsByTag, Search
 
 
 urlpatterns=[
@@ -8,4 +8,5 @@ urlpatterns=[
     path('category/<str:slug>/', PostsByCategory.as_view(), name='category'),
     path('tag/<str:slug>/', PostsByTag.as_view(), name='tag'),
     path('post/<str:slug>/', GetPost.as_view(), name='post'),
+    path('search/', Search.as_view(), name='search'),
 ]
